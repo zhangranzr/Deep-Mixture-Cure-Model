4d0e693dc03cb0f83c028251c936af9072e3c110
Ran ZHANG <zhangran0116@hotmail.com>
2020-12-29 21:14:05 +0100

    updates

--- data_preprocess.py
+++ data_preprocess.py
@@ -2,9 +2,14 @@
 #"Dynamic-DeepHit: A Deep Learning Approach for Dynamic Survival Analysis With Competing Risks Based on Longitudinal Data," 
 #IEEE Transactions on Biomedical Engineering (TBME). 2020
 
+'''
+this step helps to generate the data and mask for further analysis.
+This step will not involve standardization or normalization. 
+Such data preprocess will take place after train-validation-test split.
+'''
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import StandardScaler
+import math
 
 class preprocess_data():
 
@@ -27,8 +32,9 @@ class preprocess_data():
         input data: 
             id   : indicator of individual 
             tte  : time-to-event or time-to-censoring
-                    usually the same as the last obervation time of each individual
-            time : time at which observations are measured, starts time must be sycronized
+                   usually the same as the last obervation time of each individual
+            time : time at which observations are measured, start time is the initial recognition of loan 
+                    (in loan data, corresponding to column t)
             label: censoring information
                 - 0: censoring
                 - 1: event type 1
@@ -39,49 +45,44 @@ class preprocess_data():
         '''
         df = self.data_original
         feat_list = self.feat_list
-
-        bin_list           = self.binary_f# ['drug', 'sex', 'ascites', 'hepatomegaly', 'spiders']
-        cont_list          = self.continuous_f #['age', 'edema', 'serBilir', 'serChol', 'albumin', 'alkaline', 'SGOT', 'platelets', 'prothrombin', 'histologic']
         
-        df                 = df[['id', 'time', 'label', 'payoff_label']+feat_list]
-
-        if not cont_list:
-            cont_list = feat_list
-        # normalize data
-        scaler = StandardScaler()
-
-        df[cont_list] = scaler.fit_transform(
-            np.asarray(df[cont_list]).astype(float)
-            )
+        df                 = df[['id', 't', 'label', 'payoff_label']+feat_list]
 
         grouped  = df.groupby(['id'])
         id_list  = pd.unique(df['id'])
-        max_measurment = np.max(grouped.count())[0]
+        max_measurment = int(math.ceil(df.t.max()*1.2)) #max duration
+
 
         data     = np.zeros([len(id_list), max_measurment, len(feat_list)+1])
-        pat_info = np.zeros([len(id_list), 5])
+        # data dimention (n_customers, durations, n_features) 
+        pat_info = np.zeros([len(id_list), 5]) 
 
         for i, tmp_id in enumerate(id_list):
             tmp = grouped.get_group(tmp_id).reset_index(drop=True)
 
+            #if payoff or not
             pat_info[i,4] = np.max(tmp['payoff_label']) 
-            pat_info[i,3] = tmp.shape[0]            # number of measurements
-            pat_info[i,2] = np.max(tmp['label'])         # cause
-            pat_info[i,1] = np.max(tmp['time'])     # time to event
-            pat_info[i,0] = tmp['id'][0]      
+            #number of measurements
+            pat_info[i,3] = tmp.shape[0]            
+            #default_info(payoff is marked as censoring) 
+            pat_info[i,2] = np.max(tmp['label'])         
+            #time of measurement, snycronized at initial loan recognition 
+            pat_info[i,1] = np.max(tmp['t'])     
+            #customer id
+            pat_info[i,0] = tmp['id'][0]                 
 
-            data[i, :int(pat_info[i, 3]), 1:]  = tmp[feat_list]
-            data[i, :int(pat_info[i, 3]-1), 0] = np.diff(tmp['time'])
+            for each_t in tmp['t']:
+                data[i, each_t, 1:]  = tmp[tmp['t'] == each_t][feat_list]
+            #data[i, :int(pat_info[i, 3]-1), 0] = np.diff(tmp['t'])
             # return the interval of diff measurement times
             
-            self.data = data
-            self.pat_info = pat_info
+        self.data = data
+        self.pat_info = pat_info
 
         return self.pat_info, self.data
 
 
-    def preprocess(self, 
-    norm_mode = 'standard'):
+    def preprocess(self):
         
         pat_info, data     = self.construct_dataset()
 
@@ -89,18 +90,12 @@ class preprocess_data():
         data_mi[np.isnan(data)]  = 1
         data[np.isnan(data)]     = 0 
 
-        #x_dim           = np.shape(data)[2] 
-        # 1 + x_dim_cont + x_dim_bin (including delta)
-        #x_dim_cont      = len(cont_list)
-        #x_dim_bin       = len(bin_list) 
-
-        
         payoff_label    = self.pat_info[:,[4]]
-        last_measurement= self.pat_info[:,[3]]  #pat_info[:, 3] contains age/month at the last measurement
+        duration        = self.pat_info[:,[3]]  #pat_info[:, 3] contains age/month at the last measurement
         label           = self.pat_info[:,[2]]  #status 0,1,2,....
         time            = self.pat_info[:,[1]]  #time when event occurred or censoring time
         
-        num_Category    = int(np.max(self.pat_info[:, 1]) * 1.2) 
+        num_Category    = data.shape[1]
         #or specifically define larger than the max tte 
         num_Event       = len(np.unique(label)) - 1
 
@@ -108,60 +103,41 @@ class preprocess_data():
             label[np.where(label!=0)] = 1 #make single risk
 
         mask1_rnn       = self.get_mask_rnn(data, pat_info)
-        mask2           = self.get_mask2(time, label, num_Event, num_Category)
-        mask3           = self.get_mask3(time, -1, num_Category)
+        mask_ranking    = self.get_mask_ranking(time, -1, num_Category)
 
         #Dimension       = (x_dim, x_dim_cont, x_dim_bin)
         DATA            = (data, time, label, payoff_label)
-        MASK            = (mask1_rnn, mask2, mask3)
-
+        MASK            = (mask1_rnn, mask_ranking)
 
         return DATA, MASK, data_mi, pat_info, #Dimension
 
     def get_mask_rnn(self, data, pat_info):
         
         '''
-        mask1 is required to get the contional probability (to calculate the denominator part)
-        mask1 size is [N, num_Event, num_Category]. 1's until the last measurement time
+        mask1 is required to get the likelihood_loss
         formula:
 
         '''
         mask_1 = np.zeros(data.shape[:2])   # comput multiply until last time point
-        for i in range(len(pat_info[:,3])):
-            mask_1[i, :int(pat_info[i,3])]      = 1
+        for i in range(len(pat_info[:,1])):
+            mask_1[i, :int(pat_info[i,1])]      = 1
 
         mask_2 = np.zeros(data.shape[:2])   # mask at the last time point
-        for i in range(len(pat_info[:,3])):
-            mask_2[i, int(pat_info[i,3]-1)]     = 1
+        for i in range(len(pat_info[:,1])):
+            mask_2[i, int(pat_info[i,1]-1)]     = 1
 
-        mask_3 = np.zeros(data.shape[:2])   # mask at the last time point
-        for i in range(len(pat_info[:,3])):
-            mask_3[i, :(int(pat_info[i,3]-1))]  = 1
+        mask_3 = np.zeros(data.shape[:2])   # mask before the last time point
+        for i in range(len(pat_info[:,1])):
+            mask_3[i, :(int(pat_info[i,1]-1))]  = 1
 
         return (mask_1, mask_2, mask_3)
 
-    def get_mask2(self, time, label, num_Event, num_Category):
+    def get_mask_ranking(self, time, meas_time, num_Category):
         '''
-        mask2 is required to get the log-likelihood loss 
-        mask2 size is [N, num_Event, num_Category]
-        if not censored : one element = 1 (0 elsewhere)
-        if censored     : fill elements with 1 after the censoring time (for all events)
-        '''
-        mask = np.zeros([np.shape(time)[0], num_Event, num_Category]) # for the first loss function
-        for i in range(np.shape(time)[0]):
-            if label[i,0] != 0:  #not censored
-                mask[i,int(label[i,0]-1),int(time[i,0])] = 1
-            else: #label[i,2]==0: censored
-                mask[i,:,int(time[i,0]+1):] =  1 #fill 1 until from the censoring time (to get 1 - \sum F)
-        return mask
-
-    def get_mask3(self, time, meas_time, num_Category):
-        '''
-        mask5 is required calculate the ranking loss (for pair-wise comparision)
-        mask5 size is [N, num_Category]. 
+            mask5 is required calculate the ranking loss (for pair-wise comparision)
+            mask5 size is [N, num_Category]. 
             - For longitudinal measurements:
                 1's from the last measurement to the event time (exclusive and inclusive, respectively)
-                denom is not needed since comparing is done over the same denom
             - For single measurement:
                 1's from start to the event time(inclusive)
         '''
--- utils_lossFun.py
+++ utils_lossFun.py
@@ -3,36 +3,40 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Input, LSTM, Reshape, Masking, Dropout, Layer
+from lifelines.utils import concordance_index
 
 class get_loss():
-    def __init__(self, batch_index, mask, OUT_pi, OUT_hi, censoring_status,
-    payoff_status, RNN_net, alpha = 0.5, _EPSILON = 1e-08):
+    def __init__(self, batch_index, pat_info, MASK, OUT_pi, OUT_hi, censoring_status,
+    payoff_status, RNN_net, alpha = 0.5, _EPSILON = 1e-08, punish_z = True,
+    sigma1 = 0.1):
         self.batch_index = batch_index
-        self.mask = mask
-        self.OUT_h_i = OUT_hi
-        self.OUT_p_i  = OUT_pi
+        self.pat_info     = pat_info
+        self.mask = MASK
+        self.OUT_h_i = OUT_hi  # conditional prob/ instantaneous probability (n_customer, n_maxtime)
+        self.OUT_p_i  = OUT_pi # instance probability (n_customer,1)
         self.RNN_net = RNN_net 
         self.censoring_status = censoring_status
         self.payoff_status = payoff_status
         self.alpha = alpha
         self.p_il = None
         self.S_t  = None
-        self.loss_likelihood_mix = None
-        self.loss_incidence = None
+        self.loss_likelihood_mix = None#
+        self.punish_z = punish_z
         self._EPSILON = _EPSILON 
+        self.sigma1 = sigma1
 
     #log_likelihood preparation
     def Prob_Surv(self):
 
         _EPSILON = self._EPSILON
-        (mask_1, mask_2, mask_3) = self.mask 
+        (mask_1, mask_2, mask_3) = self.mask[0] 
         
         mask_1 = tf.constant(mask_1[self.batch_index], shape = self.OUT_h_i.shape, dtype = 'float32')
         mask_2 = tf.constant(mask_2[self.batch_index], shape = self.OUT_h_i.shape, dtype = 'float32')
         mask_3 = tf.constant(mask_3[self.batch_index], shape = self.OUT_h_i.shape, dtype = 'float32')
 
         if self.RNN_net == False:
-            h_i = tf.nn.softmax(self.OUT_h_i, axis = 1)
+            h_i = tf.nn.softmax(self.OUT_h_i, axis = 1) 
             h_i = tf.reshape(tf.math.multiply(h_i, mask_1), mask_1.shape, name='h_i') 
             W_t = tf.clip_by_value(tf.reshape(tf.math.reduce_sum(h_i, axis=1, name='W_t'), 
             (len(self.batch_index), 1)), _EPSILON, 1-_EPSILON)
@@ -56,7 +60,7 @@ class get_loss():
             p_il = tf.clip_by_value(tf.reshape(tf.math.multiply(h_last, mul_h), 
             (len(self.batch_index), 1)), _EPSILON, 1-_EPSILON)
 
-            self.p_il = p_il
+            self.p_il = p_il # probability of event time
             self.S_t  = S_t
 
             return self.p_il, self.S_t
@@ -84,23 +88,18 @@ class get_loss():
         l_uncensored = tf.reduce_sum(
             tf.math.log(l_uncensored)
             )
-        # punishment of t>z
+        
+        # censored data, max the prob of cumulative prob until event time
         l_uncensored_2 = tf.reduce_sum(
-            tf.math.log((1-S_t)[self.censoring_status == 1])
-            )
-
-        self.loss_likelihood_mix = -(l_censored + l_uncensored + l_uncensored_2)
-
-        return self.loss_likelihood_mix 
-
-    def loss_icds(self):
-        p_i  = self.OUT_p_i
-        l_incidence  = tf.keras.losses.binary_crossentropy(
-                self.censoring_status[self.censoring_status + self.payoff_status == 1], 
-                p_i[self.censoring_status + self.payoff_status == 1])
-
-        self.loss_incidence = l_incidence
-        return self.loss_incidence
+            tf.math.log(tf.math.multiply(p_i, 1-S_t)[self.censoring_status == 1]
+            ))
+        
+        if (self.punish_z):
+            self.loss_likelihood_mix = -(l_censored*0.25 + l_uncensored*0.5 + l_uncensored_2*0.25)
+        else:
+            self.loss_likelihood_mix = -(l_censored*0.5 + l_uncensored*0.5)
+        
+        return self.loss_likelihood_mix, (l_censored, l_uncensored, l_uncensored_2)
     
     def AUC(self):
         acc   = tf.keras.metrics.AUC(int(len(self.censoring_status)/2))
@@ -109,12 +108,92 @@ class get_loss():
         )
 
         return acc.result()
-    #def ranking_loss(self):
 
+    ### LOSS-FUNCTION 2 -- Ranking loss
+    def loss_Ranking(self):
+        '''
+        compute ranking loss. 
+        return: tf.tensor
+        input(no competing risk):
+            pi_l        : probability matrix of customers dim(customers, max_time)
+            mask_ranking: 
+        '''
+        OUT_hi = self.OUT_h_i 
 
+        if self.RNN_net:
+            pil_matrix = np.zeros(OUT_hi.shape)
+            pil_matrix[:, 0] = OUT_hi[:, 0]
+            for i in range(OUT_hi.shape[-1]):
+                if i == 0:
+                    pass
+                else:
+                    temp_h = OUT_hi[:, i]
+                    temp_h_1 = tf.math.subtract(1, OUT_hi)[:, :i-1]
+                    h = tf.reduce_prod(temp_h_1, axis = 1)
+                    pil_matrix[:, i] = tf.math.multiply(temp_h, h).numpy()
+            OUT_hi = pil_matrix
+        else: 
+            pass
+
+        mask_ranking = self.mask[1]
+        sigma1 = self.sigma1
+        label = self.censoring_status
+
+        sigma1 = tf.constant(sigma1, dtype=tf.float32)
+        label  = tf.constant(label,  dtype=tf.dtypes.float32)
+        mask_ranking = tf.constant(mask_ranking,  dtype=tf.dtypes.float32)
+        
+        one_vector = tf.ones_like(label, dtype=tf.float32)
+
+        tmp_e = tf.reshape(OUT_hi, shape = mask_ranking.shape)
+        R = tf.matmul(tmp_e, tf.transpose(mask_ranking)) 
+        # cummulative incidence prob at actual event time
+        diag_R = tf.reshape(tf.linalg.diag_part(R), [-1, 1]) 
+        R = tf.matmul(one_vector, tf.transpose(diag_R)) - R 
+        R = tf.transpose(R)  
+        T = tf.nn.relu(tf.sign(tf.matmul(one_vector, tf.transpose(label)) - tf.matmul(label, tf.transpose(one_vector))))
+        eta = tf.reduce_sum(tf.reduce_mean(T * tf.exp(-R/sigma1), axis=1, keepdims=True))
+        return eta
     
-    #def loss_total(self):
+    def ANLP(self):
 
+        Prob, S_t = self.Prob_Surv()
+
+        P = tf.reduce_sum(
+            tf.math.log(Prob[self.censoring_status == 1]
+            ))
+        P = P/sum((self.censoring_status == 1))*(-1)
+
+        return P
+
+    def c_index(self):
+
+        OUT_hi = self.OUT_h_i
+        if self.RNN_net:
+            pil_matrix = np.zeros(OUT_hi.shape)
+            pil_matrix[:, 0] = OUT_hi[:, 0]
+            for i in range(OUT_hi.shape[-1]):
+                if i == 0:
+                    pass
+                else:
+                    temp_h = OUT_hi[:, i]
+                    temp_h_1 = tf.math.subtract(1, OUT_hi)[:, :i-1]
+                    h = tf.reduce_prod(temp_h_1, axis = 1)
+                    pil_matrix[:, i] = tf.math.multiply(temp_h, h).numpy()
+        else:
+            pil_matrix = OUT_hi
+
+        events = self.pat_info[:, 3][self.batch_index]
+        pred_cindex = []
+        preds_t = []
+
+        for i in [12, 24, 36, OUT_hi.shape[-1]]:
+            preds = tf.math.argmax(pil_matrix[:, :i], axis = 1).numpy()
+            res = concordance_index(events, preds)
+            pred_cindex.append(res)
+            preds_t.append(preds)
+
+        return pred_cindex, preds_t, pil_matrix, 
         
 
 
